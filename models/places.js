a32a8da7e7e9ce125320cba81f58ea4a1561ebb3
John <john.diket@gmail.com>
2021-09-22 11:51:15 -0400

    place model created and exported

---
+++ models/places.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const placeSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    phone: {type: String},
+    address: {
+        street: {type: String},
+        city: {type: String},
+        state: {type: String},
+        zip: {type: Number}
+    }
+});
+
+module.exports = mongoose.model('Place', placeSchema);
