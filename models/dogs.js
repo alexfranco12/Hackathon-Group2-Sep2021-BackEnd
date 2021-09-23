502352a584c99060056ff1628dea32acd49bad14
John <john.diket@gmail.com>
2021-09-22 22:03:12 -0400

    altered photo key value to an array for multiple photos

--- models/dogs.js
+++ models/dogs.js
@@ -4,7 +4,7 @@ const dogSchema = mongoose.Schema({
     name: {type: String, title: 'Name', required: true},
     location: mongoose.ObjectId,
     age: {type: Number, title: 'Age', required: true},
-    photo: {type: String, title: 'Photo'},
+    photo: [{type: String, title: 'Photo'}],
     description: {type: String, title: 'Description'},
     breed: [{ type: String, required: true }],
     size: {type: String, enum: ['XS', 'S', 'M', 'L', 'XL', 'XXL']},
