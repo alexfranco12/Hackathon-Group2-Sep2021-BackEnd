31ba7974d67f8b166ea157295b0a38a8f21bc64b
John <john.diket@gmail.com>
2021-09-23 12:31:56 -0400

    changed database name connections

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3003;
 const MONGODB_URI = 
     process.env.NODE_ENV === "production"
     ? process.env.MONGODB_URI
-    : "mongodb://localhost/hackathon";
+    : "mongodb://localhost/dogs";
 
 // MIDDLEWARE
 app.use(express.json());
