70a80d5666bd43a8a27feab33e9f55a33c6535cd
alfranc <alexfranco12@gmail.com>
2022-02-12 20:30:22 -0600

    moved all the code to populate data into one file to reduce repeat code.

--- db/seed-data.js
+++ db/seed-data.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const Dogs = require('../models/dogs');
 const Places = require('../models/places');
 
 // Require a json file which contains some dummy data
-const seedData = require("./places-seed.json");
+const dogData = require("./dog-seed.json"); 
+const placeData = require("./places-seed.json");
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -19,9 +21,18 @@ db.on('connected', function () {
   console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
 
+Dogs.deleteMany({}).then(() => {
+  // Insert the dummy data and return it so we are able to log the data
+  return Dogs.insertMany(dogData);
+}).then(console.log)
+.catch(console.error)
+.finally(() => {
+  process.exit();
+});
+
 Places.deleteMany({}).then(() => {
   // Insert the dummy data and return it so we are able to log the data
-  return Places.insertMany(seedData);
+  return Places.insertMany(placeData);
 }).then(console.log)
 .catch(console.error)
 .finally(() => {
