2da4a92266b3713f246c66726f2fa0fee258f699
alfranc <alexfranco12@gmail.com>
2022-02-12 20:15:56 -0600

    loading in data from json instead of having data directly inside the insert function to populate database

--- db/seed_places.js
+++ db/seed_places.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const Places = require('../models/places');
+
+// Require a json file which contains some dummy data
+const seedData = require("./places-seed.json");
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -15,63 +19,11 @@ db.on('connected', function () {
   console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
 
-const Places = require('../models/places');
-
-(async function() {
-
-    
-    await Places.deleteMany({});
-    const places = await Places.create([
-        {
-            name: 'It Takes A Village',
-            phone: '910-514-4176',
-            address: {
-                street: '1200 S Horner Blvd #4701',
-                city: 'Sanford',
-                state: 'NC',
-                zip: 27331
-            },
-            distance: 6,
-            fee: 100
-        }, {
-            name: 'Cross Creek Animal Rescue',
-            phone: '919-883-4653',
-            address: {
-                street: '56 Baron Rogers Way',
-                city: 'Sanford',
-                state: 'NC',
-                zip: 27331
-            },
-            distance: 6,
-            fee: 100
-        }, {
-            name: 'Hero Shepherds Breeders',
-            phone: '910-585-3565',
-            address: {
-                street: '815 County Access 15',
-                city: 'Dunn',
-                state: 'NC',
-                zip: 28334
-            },
-            distance: 18,
-            fee: 175
-        }, {
-            name: 'Cumberland County Animal Rescue',
-            phone: '910-321-6852',
-            address: {
-                street: '4704 Corporation Dr',
-                city: 'Fayetteville',
-                state: 'NC',
-                zip: 28306
-            },
-            distance: 3,
-            fee: 55
-        }
-    ]);
-    
-
-    console.log(places)
-
-    process.exit();
-
-})();
+Places.deleteMany({}).then(() => {
+  // Insert the dummy data and return it so we are able to log the data
+  return Places.insertMany(seedData);
+}).then(console.log)
+.catch(console.error)
+.finally(() => {
+  process.exit();
+});
